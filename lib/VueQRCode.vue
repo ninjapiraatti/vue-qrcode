1a837a46d03014fdf4d152c3052a9572077d3016
ninjapiraatti <tuoppil@gmail.com>
2022-12-29 14:06:16 +0200

    Remove redundant boilerplate

--- lib/VueQRCode.vue
+++ lib/VueQRCode.vue
@@ -1,19 +1,14 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 
-defineProps({
-		msg: { type: String }
+const props = defineProps({
+		input: { type: String }
 	}
 )
 
-const count = ref(0)
 </script>
 
 <template>
-	<h1>{{ msg }}</h1>
-	<div class="card">
-		<button type="button" @click="count++">count is {{ count }}</button>
-	</div>
+	<h1>{{ props.input }}</h1>
 </template>
 
 <style scoped>
