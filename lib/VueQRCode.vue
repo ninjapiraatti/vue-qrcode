5c4f5d798881f6295558f78c60a168bf90ab3ae6
ninjapiraatti <tuoppil@gmail.com>
2023-01-02 08:38:34 +0200

    Move css to inline to improve DX

--- lib/VueQRCode.vue
+++ lib/VueQRCode.vue
@@ -1,6 +1,6 @@
 
 <template>
-	<div class="qr-container" v-html="qrCode" />
+	<div class="vue-html-qr-container" v-html="qrCode" />
 </template>
 
 <script setup lang="ts">
@@ -8,29 +8,52 @@ import { onMounted, ref } from "vue"
 import QRCode from "qrcode"
 
 const props = defineProps<{
-	input: string
+	input: string,
+	fillColor: string,
+	backgroundColor: string,
+	pixelSize: string
 }>()
+
+const defaults = {
+	fillColor: "#000",
+	backgroundColor: "transparent",
+	pixelSize: "4px"
+}
 
 const qrCode = ref()
 
 const GenerateHTMLQR = async function(input: string) {
+	if (input.length == 0) {
+		console.error("No input for QR code")
+	}
 	let qrPixels = new Uint8Array
 	try {
 		const qrObject = QRCode.create(input, { errorCorrectionLevel: "H"})
 		qrPixels = qrObject.modules.data
 	}
 	catch(e) {
-		console.log("error", e)
+		console.error("Error creating QR Code:\n", e)
 	}
 	const qrDiv = document.createElement("div")
 	const side = Math.sqrt(qrPixels.length)
 	for (let i = 0; i < side; i++) {
 		const qrRow = document.createElement("div")
+		qrRow.style.height = props.pixelSize || defaults.pixelSize
+		qrRow.style.margin = "0"
+		qrRow.style.lineHeight = "0"
 		qrRow.className = "qr-row"
 		qrDiv.appendChild(qrRow)
 		for (let j = 0; j < side; j++) {
 			const pixelDiv = document.createElement("div")
-			pixelDiv.className = qrPixels[i * side + j] == 1 ? "qr-pixel qr-pixel-black" : "qr-pixel qr-pixel-white"
+			pixelDiv.style.width = props.pixelSize || defaults.pixelSize
+			pixelDiv.style.height = props.pixelSize || defaults.pixelSize
+			pixelDiv.style.display = "inline-block"
+			if (qrPixels[i * side + j] == 1) {
+				pixelDiv.style.backgroundColor = props.fillColor || defaults.fillColor
+			} else {
+				pixelDiv.style.backgroundColor = props.backgroundColor || defaults.backgroundColor
+			}
+			pixelDiv.className = "qr-pixel"
 			qrRow.appendChild(pixelDiv)
 		}
 	}
@@ -42,30 +65,3 @@ onMounted(() => {
 })
 
 </script>
-
-<style scoped>
-.qr-container {
-	position: absolute;
-	right: 20px;
-}
-
-.qr-row {
-	margin: 0;
-	line-height: 0;
-	height: 4px;
-}
-
-.qr-pixel {
-	width: 4px;
-	height: 4px;
-	display: inline-block;
-}
-
-.qr-pixel-black {
-	background-color: #000;
-}
-
-.qr-pixel-white {
-	background-color: #fff;
-}
-</style>
